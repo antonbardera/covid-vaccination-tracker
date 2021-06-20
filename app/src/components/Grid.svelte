9bd693dfd7a2a95786b7bf6fa696133fc71e069f
Spe Chen Yu-Syuan <spechen@sph.com.sg>
2021-06-20 19:12:28 +0800

    fix data issue and tidy up code

--- app/src/components/Grid.svelte
+++ app/src/components/Grid.svelte
@@ -5,78 +5,57 @@
 
 	import * as ccaaData2 from "../../public/gap-chart-ca.json";
 	import * as allCCAA from "../../public/gap-chart-demo.json";
-	import * as data_raw from "../../public/data_xavier.json";
+	import * as data_raw from "../../public/data_xavier.json"; // downloaded from Xavier's branch
 	
+	/* --------------------  
+	   DATA PREPROCESSING 
+	-----------------------*/
 	// first vaccination date March 31
 	let DataLong = data_raw.default.filter(d => new Date(d.fecha) > new Date("2021-03-30"));
 
-	// console.log(DataLong.filter(d => d.ccaa === 'Cantabria'));
 	let uniqueCCAA = [...new Set(DataLong.map(item => item.ccaa))]
-	// console.log(DataLong.filter(d => d.ccaa === "Cataluña"))
+	console.log("CCAAs in the dataset----------");
 	console.log(uniqueCCAA);
 
-	// Totales is missing. so use "Cataluña" for now
+	// Totales is missing in xavi's dataset, so use "Cataluña" as value1 for now
 	let dataGlobal = DataLong.filter(d => d.ccaa === "Cataluña").map(d => {return{
 		date: new Date(d.fecha.split("T")[0]),
 		dateStr: d.fecha.split("T")[0],
 		ccaa: d.ccaa,
 		value1: Math.round(+d["dose2_perc_total"]) / 100
 	};})
-	// console.log('in Grid dataGlobal -----')
-	// console.log(dataGlobal);
 
 	function findValueByDate(dateStr){
 		let result = dataGlobal.filter(d => d.dateStr === dateStr).map(d => d.value1)[0]
+		// fill nan with 0 for value0 
 		if (isNaN(result)){ return 0} else {return result} 
 	}
 	let data2 = DataLong.map(d => {return{
 		date: new Date(d.fecha.split("T")[0]),
-		dateStr: d.fecha.split("T")[0],
+		dateStr: new Date(d.fecha.split("T")[0]),
 		ccaa: d.ccaa,
 		value0: (isNaN(d["dose2_perc_total"]))? 0 : Math.round(+d["dose2_perc_total"]) / 100,
 		value1: findValueByDate(d.fecha.split("T")[0])
 	};})
 
-
-	console.log(allCCAA);
-
-
-function getVaxDataByCCAA(ccaaName){
-	// return data2.filter(d => d.ccaa === ccaaName)
-	let selected = allCCAA.default.filter(d => d.ccaa === ccaaName)
-	return selected.map(d => {return{
-		date: d.date,
-		dateStr: new Date (d.dateStr),
-		ccaa: d.ccaa,
-		value0: d.value0,
-		value1: d.value1
-	};})
-
-}
-
-// console.log('Cantabria====')
-console.log(getVaxDataByCCAA('Castilla y Leon'));
-
-	// console.log(ccaaData2)	
-	let ccaaData = ccaaData2.default
-
-	// console.log(ccaaData)	
+	/* --------------------  
+	   Set up for <main> 
+	-----------------------*/
+	
 	let color = ['rgba(92,198,178, 1)', 'rgba(0,0,0, 1)']
 	let colorDiff = ['rgba(92,198,178, 0.5)', 'rgba(0,0,0, 0.5)']
-	ccaaData.forEach(d => d.dateStr = new Date(d.dateStr));
-	const loc2 = new locale('en');
+	const loc = new locale('en');
 	const format = {
-		x: loc2.formatTime('%B %e'),
-		y: loc2.format(',.2f'),
+		x: loc.formatTime('%B %e'),
+		y: loc.format(',.2f'),
 	}
-
-
 
 	$: col = `repeat(${grid[1]}, 1fr)`;
 	$: row = `repeat(${grid[0]}, 1fr)`;	
 	
 	// accent
-	var items_ = [
+	// for xavier's dataset
+	var items = [
     ['Galicia', 'Asturias', 'Cantabria', 'País Vasco', 'Aragón', 'Cataluña'],
     [0, 'Castilla y León', 'La Rioja', 'Navarra', 'Com. Valenciana', 0],
     [0, 'Extremadura', 'Madrid', 'Castilla-La Mancha', 'Murcia', 0],
@@ -84,13 +63,28 @@ console.log(getVaxDataByCCAA('Castilla y Leon'));
     ];
 
 	// to loosely match naming in data.json. some unicode problems
-	var items = [
-    ['Galicia', 'Asturias', 'Cantabria', 'País Vasco', 'Aragón', 'Cataluña'],
-    [0, 'Castilla y Leon', 'La Rioja', 'Navarra', 'Com Valenciana', 0],
-    [0, 'Extremadura', 'Madrid', 'Castilla - La Mancha', 'Murcia', 0],
-    ['Canarias',0, 'Andalucía', 0,0,'Baleares'],
-    ];
-	
+	// for demo dataset
+	// var items_ = [
+    // ['Galicia', 'Asturias', 'Cantabria', 'País Vasco', 'Aragón', 'Cataluña'],
+    // [0, 'Castilla y Leon', 'La Rioja', 'Navarra', 'Com Valenciana', 0],
+    // [0, 'Extremadura', 'Madrid', 'Castilla - La Mancha', 'Murcia', 0],
+    // ['Canarias',0, 'Andalucía', 0,0,'Baleares'],
+    // ];
+
+	function getVaxDataByCCAA(ccaaName){
+		let selected = data2.filter(d => d.ccaa === ccaaName)
+		return selected
+		// for other demo dataset
+		// let selected = allCCAA.default.filter(d => d.ccaa === ccaaName)
+		// return selected.map(d => {return{
+		// 	date: d.date,
+		// 	dateStr: new Date (d.dateStr),
+		// 	ccaa: d.ccaa,
+		// 	value0: d.value0,
+		// 	value1: d.value1
+		// };})
+	}
+
     function getItem(i,j){
 		if (items[i][j] === 0){
 			return ' '
@@ -148,7 +142,7 @@ console.log(getVaxDataByCCAA('Castilla y Leon'));
 	
 	.colored{
 		/* background-color: limegreen !important; */
-	border-style: dotted;
+		border: 2px solid lightgray;
 	}	
 	
 	.Canarias{
