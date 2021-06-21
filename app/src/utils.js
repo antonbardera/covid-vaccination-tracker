b59ebc8044922d232d62c4d441a4fcc72c0f17b6
Xavier B <xbollo@gmail.com>
2021-06-21 17:02:28 +0200

    rolling avg function

--- app/src/utils.js
+++ app/src/utils.js
@@ -1,7 +1,15 @@
-import {aq,op} from "@arquero"
+import * as aq from 'arquero';
 
 export function rollingAvg(_data){
     let data = aq.from(_data)
-        .print()
+        .fold(aq.not(['ccaa','fecha']))
+        .derive({value: aq.rolling(d=> op.average(d.value))})
+        // .reify()
+        .groupby('fecha','ccaa')
+        .pivot('key','value')
+        .objects()
+        // .print()
+    // console.log(data)
     return data
 }
+
