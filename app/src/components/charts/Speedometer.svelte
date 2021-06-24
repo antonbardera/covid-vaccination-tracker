d18eb396377695ba96687096fbe200867ec0f0a7
Spe Chen Yu-Syuan <spechen@sph.com.sg>
2021-06-24 22:39:54 +0800

    small tweaks

--- app/src/components/charts/Speedometer.svelte
+++ app/src/components/charts/Speedometer.svelte
@@ -16,10 +16,12 @@
 	textColor="{'#1E1E1E'}ª"
 	maxSegmentLabels={1}
 	customSegmentStops={[0, speedValue, 1.0]}
-	segmentColors={["#04D94F", "#CFD8DC"]}
+	segmentColors={["rgba(4, 217, 79, 0.8)", "rgba(207, 216, 220, 0.8)"]}
 	ringWidth={55}
 	needleColor={"#455A64"}
 	needleTransitionDuration={4500}
 	needleTransition="easeElastic"
 	needleHeightRatio={0.9}
 />
+
+<!-- segmentColors={["#04D94F", "#CFD8DC"]} -->
